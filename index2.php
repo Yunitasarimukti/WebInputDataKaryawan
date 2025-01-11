74c762fed5fd2ae5d6ac2b680e35cb84d1fb03ac
YunitaSari <yunitas.21@upi.edu>
2025-01-11 14:25:55 +0700

    Add files via upload

---
+++ index2.php
@@ -0,0 +1,231 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="TemplateMo">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
+
+    <title>SIIDK</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" href="assets/css/fontawesome.css">
+    <link rel="stylesheet" href="assets/css/templatemo-finance-business.css">
+    <link rel="stylesheet" href="assets/css/owl.css">
+<!--
+
+Finance Business TemplateMo
+
+https://templatemo.com/tm-545-finance-business
+
+-->
+  </head>
+
+  <body>
+
+    <!-- ***** Preloader Start ***** -->
+    <div id="preloader">
+        <div class="jumper">
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    </div>  
+    <!-- ***** Preloader End ***** -->
+
+    <!-- Header -->
+    <div class="sub-header">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8 col-xs-12">
+            <ul class="left-info">
+              <li><a href="#"><i class="fa fa-clock-o"></i>Senin-Jumat 09:00-17:00</a></li>
+              <li><a href="#"><i class="fa fa-clock-o"></i>Sabtu 09:00-15:00</a></li>
+              <li><a href="#"><i class="fa fa-phone"></i>(022)6866156</a></li>
+            </ul>
+          </div>
+          
+        </div>
+      </div>
+    </div>
+
+    <header class="">
+      <nav class="navbar navbar-expand-lg">
+        <div class="container">
+          <a class="navbar-brand" href="#"><h2>PT. SINAR PARA TARUNA</h2></a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item active">
+                <a class="nav-link" href="#top">Dashboard
+                  <span class="sr-only">(current)</span>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="./DataPacking/DataKaryawan.php">Data Packing</a>
+              </li>                           
+              <li class="nav-item">
+                <a class="nav-link" href="./DataFinishing/DataKaryawan.php">Data Finishing</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="./DataDaying/DataKaryawan.php">Data Daying</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="./DataStaff/DataKaryawan.php">Data Staff</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="./DataStaff/DataKaryawan.php">Hak-hak karyawan</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="./DataStaff/DataKaryawan.php">Kewajibabn  karyawan</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
+
+    <!-- Page Content -->
+    <!-- Banner Starts Here -->
+    <div class="main-banner header-text" id="top">
+        <div class="Modern-Slider">
+          <!-- Item -->
+          <div class="item item-1">
+            <div class="img-fill">
+              <img src = "pt.jpg"/>
+                <div class="text-content">
+                  <h6>Hii, ADMIN SELAMAT DATANG</h6>
+                  <h4>SISTEM INFORMASI<br>INPUT DATA KARYAWAN</h4>
+                  <a href="index1.php" class="filled-button">LOGOUT</a>
+                </div>
+            </div>
+          </div>
+          
+          <!-- // Item -->
+          <!-- Item -->
+          <div class="item item-1">
+            <div class="img-fill">
+            <img src = "pt.jpg"/>
+                <div class="text-content">
+                  <h6>Produk berkualitas</h6>
+                  <h4>Mempeorduksi tekstil<br>kain renda & tulle</h4>
+                 
+                </div>
+            </div>
+          </div>
+          <!-- // Item -->
+        </div>
+    </div>
+    <!-- Banner Ends Here -->
+
+    <div class="request-form">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8">
+            
+          </div>
+          <div class="col-md-4">
+           
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="services">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="section-heading">
+              <h2>Google<em>Maps</em></h2>
+             
+            </div>
+          </div>
+          <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15843.918812701682!2d107.5024144!3d-6.8930309!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x27f70ec909d31d4d!2sSinar%20Para%20Taruna.%20PT!5e0!3m2!1sid!2sid!4v1645115077277!5m2!1sid!2sid" width="100%" height="500px" frameborder="0" style="border: 2px;;" allowfullscreen="" loading="lazy"></iframe>
+
+    
+
+    <div class="more-info">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="more-info-content">
+              <div class="row">
+                <div class="col-md-6">
+                  <div class="left-image">
+                    
+                    <img src = "productivity.jpg" alt="">
+                  </div>
+                </div>
+                <div class="col-md-6 align-self-center">
+                  <div class="right-content">
+                    <h2>Profil <em>Singkat</em></h2>
+                    <p>Selama bertahun-tahun, sejarah perusahaan ditandai dengan ekspansi berkelanjutan dengan investasi ke teknologi dan mesin mutakhir. Hanya membeli mesin terbaik yang ada di tekstil rajutan lusi. Bertujuan untuk kain rajut lusi berkualitas tinggi, sistem manajemen kualitas total diterapkan di seluruh lini produksi. Pusat pelatihan karyawan, yang dikelola oleh para ahli di bidangnya masing-masing, memberikan keterampilan dan pengetahuan yang diperlukan untuk memenuhi dunia teknologi tekstil yang terus berubah serta tren dan permintaan pasar. PT. Sinar Para Taruna berkomitmen untuk memberikan yang terbaik dalam kualitas dan pelayanan prima kepada kliennya. Bersama dengan perbaikan terus-menerus dan menemukan cara-cara inovatif dalam melakukan sesuatu, Sipatatex berubah menjadi salah satu pemain utama dalam industri rajut lusi lokal maupun internasional dan mutlak.</p>
+                   
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    
+              
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+   
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+    
+    <div class="sub-footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <p>Copyright &copy; 2022 Yunitasari.
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Additional Scripts -->
+    <script src="assets/js/custom.js"></script>
+    <script src="assets/js/owl.js"></script>
+    <script src="assets/js/slick.js"></script>
+    <script src="assets/js/accordions.js"></script>
+
+    <script language = "text/Javascript"> 
+      cleared[0] = cleared[1] = cleared[2] = 0; //set a cleared flag for each field
+      function clearField(t){                   //declaring the array outside of the
+      if(! cleared[t.id]){                      // function makes it static and global
+          cleared[t.id] = 1;  // you could use true and false, but that's more typing
+          t.value='';         // with more chance of typos
+          t.style.color='#fff';
+          }
+      }
+    </script>
+
+  </body>
+</html>
